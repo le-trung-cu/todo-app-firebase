9df742fd5d0e345a2df4b280648772b7afff116a
trungcu <trungcuqn@gmail.com>
2020-06-25 22:20:05 +0700

    Updates

---
+++ precache-manifest.909711540f529549dd6d7e2ac70cf825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4452d21042c921065bdb761731e459",
+    "url": "/todo-app-firebase/index.html"
+  },
+  {
+    "revision": "769760316c693197d243",
+    "url": "/todo-app-firebase/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "b9a21cc345995512971a",
+    "url": "/todo-app-firebase/static/css/main.fa6575a1.chunk.css"
+  },
+  {
+    "revision": "769760316c693197d243",
+    "url": "/todo-app-firebase/static/js/2.004a1378.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/todo-app-firebase/static/js/2.004a1378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a21cc345995512971a",
+    "url": "/todo-app-firebase/static/js/main.e22fdd21.chunk.js"
+  },
+  {
+    "revision": "30dd62192aa8b3d635da",
+    "url": "/todo-app-firebase/static/js/runtime-main.01c674a8.js"
+  }
+]);
